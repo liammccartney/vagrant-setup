af9b7052c9817f6e60e9cdcf895346b6636560f1
Liam McCartney <liam.mccartney@gmail.com>
2016-02-12 18:07:33 -0500

    Add setup.sh for ne vagrant vms

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd ~
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+echo "source ~/.bash-git-prompt/gitprompt.sh\nGIT_PROMPT_ONLY_IN_REPO=1\nGIT_PROMPT_FETCH_REMOTE_STATUS=0" >> .bashrc
+sudo pip install virtualenvwrapper
+echo "export WORKON_HOME=$HOME/.virtualenvs\nsource /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
+echo "alias reload='source ~/.bashrc'" > .bash_aliases
+sudo apt-get install tmux
+echo "unbind C-b\nset -g prefix C-s\nset -g mode-mouse on\nset -g mouse-resize-pane on\nset -g mouse-select-pane on\nset -g mouse-select-window on" > .tmux.conf
+source ~/.bashrc
+tmux
+tmux source-file ~/.tmux.conf
