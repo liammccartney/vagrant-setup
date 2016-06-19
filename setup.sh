92f83b0c15bd3c1606adc1a73bad8defea8ba6c1
Liam McCartney <liam.mccartney@gmail.com>
2016-06-19 15:11:59 -0400

    Use `printf` instead of `echo`

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd ~
-echo "export EDITOR=vim" >> .bashrc
+printf "export EDITOR=vim" >> .bashrc
 git config --global user.name "Liam McCartney"
 git config --global user.email liam.mccartney@gmail.com
 git config --global core.editor "vim"
@@ -11,12 +11,12 @@ git config --global color.status.added yellow
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
-echo "source ~/.bash-git-prompt/gitprompt.sh\nGIT_PROMPT_ONLY_IN_REPO=1\nGIT_PROMPT_FETCH_REMOTE_STATUS=0" >> .bashrc
+printf "source ~/.bash-git-prompt/gitprompt.sh\nGIT_PROMPT_ONLY_IN_REPO=1\nGIT_PROMPT_FETCH_REMOTE_STATUS=0" >> .bashrc
 sudo pip install virtualenvwrapper
-echo "export WORKON_HOME=$HOME/.virtualenvs\nsource /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
-echo "alias reload='source ~/.bashrc'\nalias gs='git status'\nalias gd='git diff'" > .bash_aliases
+printf "export WORKON_HOME=$HOME/.virtualenvs\nsource /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
+printf "alias reload='source ~/.bashrc'\nalias gs='git status'\nalias gd='git diff'" > .bash_aliases
 sudo apt-get install tmux
-echo "unbind C-b\nset -g prefix C-s\nset -g default-terminal 'screen-256color'\nset -g mode-mouse on\nset -g mouse-resize-pane on\nset -g mouse-select-pane on\nset -g mouse-select-window on\nbind-key - split-window -v -c '#{pane_current_path}'
+printf "unbind C-b\nset -g prefix C-s\nset -g default-terminal 'screen-256color'\nset -g mode-mouse on\nset -g mouse-resize-pane on\nset -g mouse-select-pane on\nset -g mouse-select-window on\nbind-key - split-window -v -c '#{pane_current_path}'
 \nbind-key / split-window -h -c '#{pane_current_path}'" > .tmux.conf
 source ~/.bashrc
 tmux
